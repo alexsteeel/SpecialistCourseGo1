008d260b7f3255c1b117a9ae3b5b21e2cf5657f1
alexsteeel <bodrov.as.1989@gmail.com>
2024-09-30 16:04:12 +0300

    update practice 3

--- Day1/Practice/Practice3/main.go
+++ Day1/Practice/Practice3/main.go
@@ -7,9 +7,7 @@ func main() {
 	fmt.Println("Введите трехзначное число")
 	fmt.Scan(&number)
 
-	if number >= 1000 || number < -999 {
-		fmt.Println("Число должно быть в диапазоне от -999 до 999")
-	} else {
+	if (number >= -999 && number < -99) || (number > 99 && number <= 999) {
 		if number < 0 {
 			number = -number
 		}
@@ -17,5 +15,7 @@ func main() {
 		fmt.Println("Первая цифра:", number/100)
 		fmt.Println("Вторая цифра:", number/10%10)
 		fmt.Println("Третья цифра:", number%10)
+	} else {
+		fmt.Println("Число должно быть в диапазоне от -999 до 999 и из трех цифр.")
 	}
 }
